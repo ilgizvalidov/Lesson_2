f178daca8844cf963ccb4388a306cb67db06ca47
Ilgiz Validov <ilgiz.validov@gmail.com>
2022-09-08 12:40:21 +0300

    создали метод по заполнению масссива рандомными числами от 1 до 10, создали метод по выводу элементов массива, создали метод по нахождению индекса заданного числа если он встречается в массиве

---
+++ Ex004_ArrayLibrary/Program.cs
@@ -0,0 +1,46 @@
+﻿void FillArray (int [] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection [index] = new Random ().Next (1,10);
+        index ++;
+    }
+}
+
+void PrintArray (int [] col)
+{
+    int count = col.Length;
+    int pisition = 0;
+    while (pisition < count)
+    {
+        Console.WriteLine (col [pisition]);
+        pisition ++;
+    }
+}
+int IndexOf (int [] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int pisition = -1;
+    while (index < count)
+    {
+        if (collection [index] == find)
+        {
+            pisition = index;
+            break;
+        }
+        index ++;
+    }
+    return pisition;
+}
+
+int [] array = new int [10];
+
+
+FillArray (array);
+PrintArray (array);
+Console.WriteLine ();
+int pos = IndexOf (array, 84);
+Console.WriteLine (pos);
